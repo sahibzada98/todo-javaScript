5d332f613c2b3de6311fbd5229fa16ce66084ca5
sahibzada98 <s.ahmadmasood@gmail.com>
2020-05-26 13:44:30 -0400

    added toggle All functionality

--- object.js
+++ object.js
@@ -46,13 +46,41 @@ var todoList = {
 		var todo = this.todos[index];
 		todo.completed = !todo.completed
 		this.displayList()
+	},
+	toggleAll: function(){
+		//if everything is true, make everything false; otherwise make everything true;
+		// so the only case where we change everything to false is when all is true
+		// we can keep a boolean to check if everything is true or not; set to true; 
+		// changes to false as soon as smth is false;
+		// then we put an if(true) loop thru and make all false; else all true
+		check = true
+		for(i = 0; i < this.todos.length; i++){
+			if(this.todos[i].completed == false) check = false
+		}
+		if(check){
+			for(i = 0; i < this.todos.length; i++){
+				this.todos[i].completed = false
+			}
+		} else {
+			for(i = 0; i < this.todos.length; i++){
+				this.todos[i].completed = true
+			}
+		}
+		this.displayList()
 	}
 
 };
 
+// testing script begins here
 
 todoList.displayList()
-todoList.addToList('item 5')
+todoList.addToList('item 1')
 todoList.toggleCompleted(0)
-todoList.changeList(0, 'item 1 updated')
-todoList.deleteList(0,1)
+todoList.addToList('item 2')
+todoList.addToList('item 3')
+todoList.toggleCompleted(1)
+todoList.toggleCompleted(2)
+todoList.toggleAll()
+todoList.toggleAll()
+//todoList.changeList(0, 'item 1 updated')
+//todoList.deleteList(0,1)
